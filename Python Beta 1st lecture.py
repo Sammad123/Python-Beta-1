961076fa3e7b57c34f9e4669f349f58e9212e230
Abdul Sammad <98667516+Sammad123@users.noreply.github.com>
2022-03-01 00:11:16 +0500

    Python Beta Tricks

---
+++ Python Beta 1st lecture.py
@@ -0,0 +1,2549 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Allah_names = 99
+
+
+# In[ ]:
+
+
+Allah_names
+
+
+# In[ ]:
+
+
+alnafi_poratal = 1 
+alnafi_poratal = ["cissp","oscp"]
+
+
+# In[ ]:
+
+
+alnafi_poratal
+
+
+# In[ ]:
+
+
+a = 10
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a = 20
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a = a+a
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a = a+a
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a = a+a
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+#strings
+"islam"
+
+
+# In[ ]:
+
+
+"islam is a religion of piece "
+
+
+# In[ ]:
+
+
+"i'am going to teach python"
+
+
+# In[ ]:
+
+
+print ("I'am going to learn python")
+
+
+# In[ ]:
+
+
+print ("Islam is a reigion of piece")
+
+
+# In[ ]:
+
+
+print("islam is a religion of piece")
+print("I want to learn python")
+
+
+# In[ ]:
+
+
+print("islam is a religion \nof piece")
+
+
+# In[ ]:
+
+
+print("islam is a religion \t of piece")
+
+
+# In[ ]:
+
+
+len ("islam")
+
+
+# In[ ]:
+
+
+len("I am")
+
+
+# In[ ]:
+
+
+#String Slicing
+my_string = "islam is a religion of piece"
+
+
+# In[ ]:
+
+
+my_string
+
+
+# In[ ]:
+
+
+my_string[0]
+
+
+# In[ ]:
+
+
+my_string[3]
+
+
+# In[ ]:
+
+
+my_string[3]
+
+
+# In[ ]:
+
+
+my_string[4]
+
+
+# In[ ]:
+
+
+my_string[5]
+
+
+# In[ ]:
+
+
+my_string[-1]
+
+
+# In[ ]:
+
+
+my_string[-2]
+
+
+# In[ ]:
+
+
+my_string= "Islam"
+
+
+# In[ ]:
+
+
+my_string
+
+
+# In[ ]:
+
+
+my_string[-2]
+
+
+# In[ ]:
+
+
+my_string[-3]
+
+
+# In[ ]:
+
+
+my_string[-5]
+
+
+# In[ ]:
+
+
+my_string[-2]
+
+
+# In[ ]:
+
+
+my_string = "sammad"
+
+
+# In[ ]:
+
+
+my_string 
+
+
+# In[ ]:
+
+
+my_string[2]
+
+
+# In[ ]:
+
+
+my_string[:3]
+
+
+# In[ ]:
+
+
+my_string[0:]
+
+
+# In[ ]:
+
+
+my_string[0:5:2]
+
+
+# In[ ]:
+
+
+my_string = "Abdullah"
+
+
+# In[ ]:
+
+
+my_string
+
+
+# In[ ]:
+
+
+my_string[2:]
+
+
+# In[ ]:
+
+
+my_string[::]
+
+
+# In[ ]:
+
+
+my_string[::3]
+
+
+# In[ ]:
+
+
+my_string[::-2]
+
+
+# In[ ]:
+
+
+name = "Usman"
+
+
+# In[ ]:
+
+
+name [0] = "I"
+
+
+# In[ ]:
+
+
+last_letters = name[1:]
+
+
+# In[ ]:
+
+
+last_letters
+
+
+# In[ ]:
+
+
+"U"+ last_letters
+
+
+# In[ ]:
+
+
+"I"+ last_letters
+
+
+# In[ ]:
+
+
+a = "Salam"
+
+
+# In[ ]:
+
+
+a + " means peace!"
+
+
+# In[ ]:
+
+
+a = a + " means peace!"
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Paython and string
+letter = "a"
+
+
+# In[ ]:
+
+
+letter * 5
+
+
+# In[ ]:
+
+
+5 + 5
+
+
+# In[ ]:
+
+
+"5" + "5"
+
+
+# In[ ]:
+
+
+x = "Al Nafi"
+
+
+# In[ ]:
+
+
+x
+
+
+# In[ ]:
+
+
+x.upper()
+
+
+# In[ ]:
+
+
+x.lower()
+
+
+# In[ ]:
+
+
+x .upper()
+
+
+# In[ ]:
+
+
+#Split A STRING
+x
+
+
+# In[ ]:
+
+
+x.split()
+
+
+# In[ ]:
+
+
+x 
+
+
+# In[ ]:
+
+
+x = "Islam is a religion of peace"
+
+
+# In[ ]:
+
+
+x
+
+
+# In[ ]:
+
+
+x.split()
+
+
+# In[ ]:
+
+
+x.split("s")
+
+
+# In[ ]:
+
+
+x.split("i")
+
+
+# In[ ]:
+
+
+x
+
+
+# In[ ]:
+
+
+print ("Al nafi is a" )
+
+
+# In[ ]:
+
+
+print ("Alnafi is a {}".format ("good institute"))
+
+
+# In[ ]:
+
+
+print ("Alnafi {} {} {} {}".format ("is","a","good","institute"))
+
+
+# In[ ]:
+
+
+print ("Alnafi {1} {1} {1} {1}".format ("is","a","good","institute"))
+
+
+# In[ ]:
+
+
+print ("Alnafi {0} {1} {2} {3}".format ("is","a","good","institute"))
+
+
+# In[ ]:
+
+
+#floating format method
+result("= 100/889")
+
+
+# In[ ]:
+
+
+result
+
+
+# In[ ]:
+
+
+print ("The result was {}".format(result))
+
+
+# In[ ]:
+
+
+print ("The result was {r:3f}".format(result))
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+name = "sammad"
+
+
+# In[ ]:
+
+
+name
+
+
+# In[ ]:
+
+
+print (f"Salam, my name is {name}")
+
+
+# In[ ]:
+
+
+name= "sammad"
+age = 21
+
+
+# In[ ]:
+
+
+print (f" {name} is {age} years old.")
+
+
+# In[ ]:
+
+
+my_list =[1,2,3,]
+
+
+# In[ ]:
+
+
+my_list = ["sammad ",100,100.34]
+
+
+# In[ ]:
+
+
+len(my_list)
+
+
+# In[ ]:
+
+
+my_list = ["Ali","Usman","Abubakar"]
+
+
+# In[ ]:
+
+
+my_list[0]
+
+
+# In[ ]:
+
+
+my_list[2
+       ]
+
+
+# In[ ]:
+
+
+my_list[0:]
+
+
+# In[ ]:
+
+
+my_list[1:]
+
+
+# In[ ]:
+
+
+my_list[0:2:1]
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+my_list = ["Ali","Usman","Abubakar"]
+
+
+# In[ ]:
+
+
+second_list = ["Umer","Hamza"]
+
+
+# In[ ]:
+
+
+my_list+second_list
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+second_list
+
+
+# In[ ]:
+
+
+newlist = my_list+second_list
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist[0] = "Hazrat Ali"
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.append("Abdullah ibn masood")
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.append("Abdullah ibn Umer")
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.pop
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.pop()
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.pop()
+
+
+# In[ ]:
+
+
+newlist.pop()
+
+
+# In[ ]:
+
+
+newlist.pop()
+
+
+# In[ ]:
+
+
+newlist.pop()
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+popped_item = newlist.pop()
+
+
+# In[ ]:
+
+
+popped_item
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+popped_item
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.pop(0)
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist = ["a","c","e","b","d"]
+num_list = ["1","4","3","2","5"]
+
+
+# In[ ]:
+
+
+newlist.sort()
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+num_list.sort()
+
+
+# In[ ]:
+
+
+num_list
+
+
+# In[ ]:
+
+
+num_list.reverse()
+
+
+# In[ ]:
+
+
+num_list
+
+
+# In[ ]:
+
+
+newlist.reverse
+
+
+# In[ ]:
+
+
+newlist.reverse()
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+newlist.sort()
+
+
+# In[ ]:
+
+
+newlist
+
+
+# In[ ]:
+
+
+num_list.sort()
+
+
+# In[ ]:
+
+
+num_list
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+dict_1
+
+
+# In[ ]:
+
+
+dict_1["key1"]
+
+
+# In[ ]:
+
+
+dict_1
+
+
+# In[ ]:
+
+
+dict_1 ["Key1"]
+
+
+# In[ ]:
+
+
+dict_1
+
+
+# In[ ]:
+
+
+dict_1 = {"Key1":"Value1","Key2":"Value2"}
+
+
+# In[ ]:
+
+
+dict_1
+
+
+# In[ ]:
+
+
+dict_1["Key1"]
+
+
+# In[ ]:
+
+
+prices_laptop = {"pencil":100 ,"journal":200 , "box": 200.2}
+
+
+# In[ ]:
+
+
+prices_laptop
+
+
+# In[ ]:
+
+
+prices_laptop["pencil"]
+
+
+# In[ ]:
+
+
+prices_laptop["journal"]
+
+
+# In[ ]:
+
+
+prices_laptop["box"]
+
+
+# In[ ]:
+
+
+dic = {"k1":312,"k2":[0,1,2,3,4] ,"k3":{"2key":200}}
+
+
+# In[ ]:
+
+
+dic
+
+
+# In[ ]:
+
+
+dic ["k1"]
+
+
+# In[ ]:
+
+
+dic ["k2"]
+
+
+# In[ ]:
+
+
+dic ["k3"]
+
+
+# In[ ]:
+
+
+dic ["k3"]["2key"]
+
+
+# In[ ]:
+
+
+dict1 = {"key1":["ali","usman","abubakar"]}
+
+
+# In[ ]:
+
+
+dict1 ["key1"][2]
+
+
+# In[ ]:
+
+
+mylist = dict1["key1"]
+
+
+# In[ ]:
+
+
+mylist
+
+
+# In[ ]:
+
+
+name  = mylist[2]
+
+
+# In[ ]:
+
+
+name
+
+
+# In[ ]:
+
+
+name.upper()
+
+
+# In[ ]:
+
+
+dict1 ["key1"][2].upper()
+
+
+# In[ ]:
+
+
+dict1 
+
+
+# In[ ]:
+
+
+dic2  ={"key1":"abubakar","key2":"umer","key3":"usman"}
+
+
+# In[ ]:
+
+
+dic2
+
+
+# In[ ]:
+
+
+dic2["key4"] = 'Hamza'
+
+
+# In[ ]:
+
+
+dic2
+
+
+# In[ ]:
+
+
+dic2 ["key1"] = "Sammad"
+
+
+# In[ ]:
+
+
+dic2
+
+
+# In[ ]:
+
+
+dic2.keys()
+
+
+# In[ ]:
+
+
+dic2.values()
+
+
+# In[ ]:
+
+
+dic2.items()
+
+
+# In[ ]:
+
+
+#TUPLE
+t = (1,2,3)
+
+
+# In[ ]:
+
+
+mylist = [1,2,3]
+
+
+# In[ ]:
+
+
+type(t)
+
+
+# In[ ]:
+
+
+type(mylist)
+
+
+# In[ ]:
+
+
+len(t)
+
+
+# In[ ]:
+
+
+t = ("one ",2,3)
+
+
+# In[ ]:
+
+
+t
+
+
+# In[ ]:
+
+
+t[0]
+
+
+# In[ ]:
+
+
+t[1]
+
+
+# In[ ]:
+
+
+t[0:]
+
+
+# In[ ]:
+
+
+t[-1]
+
+
+# In[ ]:
+
+
+t[-1:]
+
+
+# In[ ]:
+
+
+t[-1:]
+
+
+# In[ ]:
+
+
+t = ("alnafi", "alnafi" ,"paython","cissp ","alnafi")
+
+
+# In[ ]:
+
+
+t.count("alnafi")
+
+
+# In[ ]:
+
+
+t.count("ciss")
+
+
+# In[ ]:
+
+
+t
+
+
+# In[ ]:
+
+
+t = ("alnafi", "alnafi" ,"paython","cissp ","alnafi")
+
+
+# In[ ]:
+
+
+t.index("alnafi")
+
+
+# In[ ]:
+
+
+t.index("cissp")
+
+
+# In[ ]:
+
+
+t.index("alnafi")
+
+
+# In[ ]:
+
+
+t.index("cissp")
+
+
+# In[ ]:
+
+
+t = ("alnafi", "alnafi" ,"paython","cissp ","alnafi")
+
+
+# In[ ]:
+
+
+t.index("paython")
+
+
+# In[ ]:
+
+
+mylist = [1,2,3]
+
+
+# In[ ]:
+
+
+mylist
+
+
+# In[ ]:
+
+
+mylist[0] = "alnafi"
+
+
+# In[ ]:
+
+
+mylist
+
+
+# In[ ]:
+
+
+t = (1,2,3,4,5)
+
+
+# In[ ]:
+
+
+t
+
+
+# In[ ]:
+
+
+t [0] = "alnafi"
+
+
+# In[ ]:
+
+
+myset = ()
+
+
+# In[ ]:
+
+
+myset
+
+
+# In[ ]:
+
+
+myset.add(1)
+
+
+# In[ ]:
+
+
+myset
+
+
+# In[ ]:
+
+
+mylist = [1,1,1,1,2,3,3,4,4,4,4,45,5,5]
+
+
+# In[ ]:
+
+
+set(mylist)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+True 
+
+
+# In[ ]:
+
+
+False 
+
+
+# In[ ]:
+
+
+type(True)
+
+
+# In[ ]:
+
+
+1>2
+
+
+# In[ ]:
+
+
+1==1
+
+
+# In[ ]:
+
+
+a = None
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('writefile', 'myfile.txt', '\nAsllamualikum wah Rahmatuaalh    \n    This is alnafi paython Beta 109 lecture\n         and we are going to leran input and output function')
+
+
+# In[ ]:
+
+
+myfile  = open("myfile.txt")
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.seek(0)
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.readline
+
+
+# In[ ]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.seek(0)
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.readlines()
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.seek(0)
+
+
+# In[ ]:
+
+
+myfile.read()
+
+
+# In[ ]:
+
+
+myfile.readline()
+
+
+# In[ ]:
+
+
+myfile.seek(0)
+
+
+# In[ ]:
+
+
+myfile.readline()
+
+
+# In[ ]:
+
+
+2 == 2
+
+
+# 2 ==3
+
+# In[ ]:
+
+
+2 ==3
+
+
+# 
+
+# In[ ]:
+
+
+2<3
+
+
+# In[ ]:
+
+
+"salam" == "sa11lam"
+
+
+# In[ ]:
+
+
+2>2
+
+
+# In[ ]:
+
+
+2!= 1
+
+
+# In[ ]:
+
+
+2!=2
+
+
+# In[ ]:
+
+
+2 >= 3
+
+
+# In[ ]:
+
+
+2 <=1
+
+
+# In[ ]:
+
+
+5 ==5
+
+
+# In[ ]:
+
+
+1 <2 
+
+
+# In[ ]:
+
+
+2 <3
+
+
+# In[ ]:
+
+
+1<2<3
+
+
+# In[ ]:
+
+
+1<2 >3
+
+
+# In[ ]:
+
+
+1<2>3
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+1 < 2 and 2 > 3
+
+
+# In[ ]:
+
+
+"Alnafi" == "Alnafi" and 9 == 9
+
+
+# In[ ]:
+
+
+1000 == 1 or 2 == 3
+
+
+# In[ ]:
+
+
+1==1 
+
+
+# In[ ]:
+
+
+not 1==1
+
+
+# In[ ]:
+
+
+not 1==2
+
+
+# In[ ]:
+
+
+if True:
+    print ("Its true")
+
+
+# In[ ]:
+
+
+tired = False
+
+if tired:
+    print ("Sleep")
+else : 
+    print ("Get back to work")
+
+
+# In[ ]:
+
+
+location = "Resturant"
+
+if location == "Resturant":
+    print("Eat at sharfoo's resturant")
+elif location == "Bank":
+    print("Get some money from the atm")
+elif location == "Bakery":
+    print("buy some eggs as they are on discount")
+elif location =="Masjid":
+    print("Pray yor salah")
+else:  
+    print("Go back to your home")
+    
+    
+
+
+# In[ ]:
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+
+# In[ ]:
+
+
+for num in my_list:
+    print(num)
+
+
+# In[ ]:
+
+
+#check gor even number
+for num in my_list:
+    if num % 2 == 0:
+        print(num)
+    else :
+        print(f"odd numbber: {num}")
+
+
+# In[ ]:
+
+
+list_sum = 0 
+for num in my_list:
+    list_sum = list_sum + num 
+    
+print(list_sum)
+
+
+# In[ ]:
+
+
+mystring = "Islam is a religion of piece"
+for letter in mystring:
+    print(letter)
+
+
+# In[ ]:
+
+
+mytupple = {1,2,3,4,5,}
+for item in mytupple:
+    print (item)
+
+
+# In[ ]:
+
+
+second_list = [(1,2),(3,5),(5,6),(7,8),(9,10)]
+
+
+# In[ ]:
+
+
+len(second_list)
+
+
+# In[ ]:
+
+
+for (a,b) in second_list:
+    print(a,b)
+    
+
+
+# In[ ]:
+
+
+third_list = [(1,2,3),(4,5,6),(7,8,9),(10,11,12)]
+
+
+# In[ ]:
+
+
+len(third_list)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+for (a,b,c) in third_list:
+    print(a)
+    print(b)
+    print(c)
+
+
+# In[ ]:
+
+
+my_dict = {"key1":1,"key2":2,"key3":3}
+
+
+# In[ ]:
+
+
+for key,value in my_dict.items():
+    print(key)
+    
+
+
+# In[ ]:
+
+
+mylist = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+
+# In[ ]:
+
+
+for num in mylist:
+    print(num)
+
+
+# In[ ]:
+
+
+#check for even number
+for num in mylist:
+    if num% 2 ==0:
+        print(num)
+    else:
+        print(f'odd number: {num}')
+
+
+# In[ ]:
+
+
+list_sum = 0 
+
+for num in mylist:
+    list_sum = list_sum + num
+print(list_sum)
+
+
+# In[ ]:
+
+
+list_sum = 0 
+
+for num in mylist:
+    list_sum = list_sum + num
+    print(list_sum)
+
+
+# In[ ]:
+
+
+mystring = "Islam is a religion of piece"
+for letter in mystring:
+    print(letter)
+
+
+# In[ ]:
+
+
+mytuple = (1,2,3,4,5)
+
+
+# In[ ]:
+
+
+for item in mytuple:
+    print(item)
+
+
+# In[ ]:
+
+
+x = 0 
+while x<10:
+    print(f'The current value of x is {x}')
+    x = x+1
+else:
+    print("X is not less than 10")
+
+
+# In[ ]:
+
+
+mysting = "Alnafi"
+
+
+# In[ ]:
+
+
+for letter in mysting:
+    if letter  == "f":
+        break 
+    print(letter)
+
+
+# In[ ]:
+
+
+my_string = "Islam"
+
+
+# In[ ]:
+
+
+my_list = []
+for letters in my_string:
+    my_list.append(letters)
+ 
+
+
+# In[ ]:
+
+
+my_list 
+
+
+# In[ ]:
+
+
+my_list = [letters for letters in my_string:]
+letters for letters in my_string:
+    
+ 
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+my_list = [letters for letters in my_string]
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+my_string = "Pakistan"
+
+
+# In[ ]:
+
+
+my_string = [a for a in my_string]
+
+
+# In[ ]:
+
+
+my_string
+
+
+# In[ ]:
+
+
+my_numbers = [x**2 for x in range (0,10) if x%2==0]
+
+
+# In[ ]:
+
+
+my_numbers
+
+
+# In[ ]:
+
+
+my_list = [1,2,3,4]
+
+
+# In[ ]:
+
+
+my_list.append(5)
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+my_list.pop()
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+my_list.reverse()
+
+
+# In[ ]:
+
+
+my_list
+
+
+# In[ ]:
+
+
+help (my_list.index)
+
+
+# # introduction to paython level one
+# 
+
+# In[ ]:
+
+
+x= "Bismillah"
+y = 1
+z= True
+
+
+# In[ ]:
+
+
+print(x,y,z)
+
+
+# In[ ]:
+
+
+x,y,z = "Orange ","Banana","Cherry"
+
+
+# In[ ]:
+
+
+print (x,y,z)
+
+
+# In[ ]:
+
+
+x=y=z = "Orange ",6,True
+
+
+# In[ ]:
+
+
+print(x,y,z)
+
+
+# In[ ]:
+
+
+a= 23
+b= 89
+c = a+b
+
+
+# In[ ]:
+
+
+c
+
+
+# In[7]:
+
+
+summation = c + "Hello"
+
+
+# In[8]:
+
+
+summation
+
+
+# In[22]:
+
+
+S = "sammadkhan" 
+
+
+# In[23]:
+
+
+R = (0,20)
+
+
+# In[24]:
+
+
+range (R)
+
+
+# In[28]:
+
+
+S = [1,2,3,4,6 ,9,6,7,4,3,4,7,5]
+
+
+# In[29]:
+
+
+sorted(S)
+
+
+# In[31]:
+
+
+S = 5
+
+
+# In[32]:
+
+
+pow(5*2)
+
+
+# In[35]:
+
+
+5**2
+
+
+# In[36]:
+
+
+S = -44.456
+
+
+# In[37]:
+
+
+abs(S)
+
+
+# In[38]:
+
+
+my_list = [value for value in range(20)]
+
+
+# In[39]:
+
+
+my_list
+
+
+# In[40]:
+
+
+len(my_list)
+
+
+# In[44]:
+
+
+len(my_list)
+
+
+# In[45]:
+
+
+my_list.append(40)
+
+
+# In[46]:
+
+
+my_list
+
+
+# In[47]:
+
+
+my_list.insert(1,30)
+
+
+# In[48]:
+
+
+my_list
+
+
+# In[50]:
+
+
+len(my_list)
+
+
+# In[51]:
+
+
+my_list.pop(1)
+
+
+# In[52]:
+
+
+my_list
+
+
+# In[55]:
+
+
+dict_ = {key: value for key, value in [["Apple",12],["Banaan",12],["Orange",24]]}
+
+
+# In[56]:
+
+
+dict_
+
+
+# In[57]:
+
+
+dict_["Orange"]
+
+
+# In[58]:
+
+
+dict_["Banaan"]
+
+
+# In[59]:
+
+
+dict_["Peach"]= 32
+
+
+# In[69]:
+
+
+dict_
+
+
+# In[70]:
+
+
+v= {"tomato":12,"potatao":24,"Peach":12}
+
+
+# In[71]:
+
+
+v
+
+
+# In[74]:
+
+
+dict_.pop("Peach")
+
+
+# In[75]:
+
+
+dict_
+
+
+# In[76]:
+
+
+dict_.pop("peach")
+
+
+# In[77]:
+
+
+dict_
+
+
+# In[78]:
+
+
+my_typ= ("joe","true",23,43.2,True)
+
+
+# In[79]:
+
+
+my_typ
+
+
+# In[85]:
+
+
+my_typ[1]= 0
+
+
+# In[86]:
+
+
+x = {1,2,3,4,5}
+y = {2,4,6,8,}
+z = {1,3,5,7}
+
+
+# In[88]:
+
+
+x
+
+
+# In[89]:
+
+
+my_set = {1,2,3,4,5}
+
+
+# In[91]:
+
+
+len(my_set)
+
+
+# In[92]:
+
+
+my_set.add(6)
+
+
+# In[93]:
+
+
+my_set 
+
+
+# In[107]:
+
+
+my_set1 = {1,2,3,4,}
+my_set23 = {2,4,}
+my_set = {3,5,7}
+
+
+# In[108]:
+
+
+my_set1.intersection(my_set23)
+
+
+# In[109]:
+
+
+my_set1.union(my_set)
+
+
+# In[110]:
+
+
+my_set1.issubset(my_set)
+
+
+# In[111]:
+
+
+my_set1.issuperset(my_set)
+
+
+# In[112]:
+
+
+my_set1.isdisjoint(my_set)
+
+
+# In[121]:
+
+
+x={1,2,3,4}
+y ={2,4,6,8}
+z= {5,7}
+
+
+# In[122]:
+
+
+x.intersection(y
+              )
+
+
+# In[123]:
+
+
+x.union(y)
+
+
+# In[124]:
+
+
+x.issubset(z)
+
+
+# In[125]:
+
+
+x.issuperset(y)
+
+
+# In[126]:
+
+
+x.isdisjoint(z)
+
+
+# In[127]:
+
+
+#loop 
+
+
+
+
+# In[128]:
+
+
+names = ["ahmed","ali","Haider"]
+
+
+# In[129]:
+
+
+range(3
+     )
+
+
+# In[130]:
+
+
+for i in range(len(names)):
+    names[i] = "Mr." + names[i]
+
+
+# In[131]:
+
+
+names
+
+
+# In[133]:
+
+
+my_fruits = ["Aplle","Banana","Orange"]
+for fruit in my_fruits:
+    print("I love",fruit)
+
+
+# In[135]:
+
+
+num = [11,12,13,14]
+for i, n in enumerate(num):
+    print("The number", n, "is present at index",i)
+
+
+# # Error Handling
+# 
+
+# In[136]:
+
+
+my_numbers = [1,2,3,4]
+print("The the lenght of the list is :",len(my_numbers))
+print("ythe code has executed")
+my_numbers[4]
+try:
+    
+
+
+# In[140]:
+
+
+def check_type(year):
+    assert type(year) == int, "The type of year is not interger"
+    print("This type year is valid")
+    print ("Successful")
+    return True
+
+
+# In[141]:
+
+
+check_type(2010)
+
+
+# In[1]:
+
+
+1
+2
+3
+4
+5
+6
+character 
+
+
+# In[4]:
+
+
+data = []
+f = open("Testfile.txt","r")
+for row in f:
+    r = row.str(ing("\n")
+    f.close()
+
+
+# In[8]:
+
+
+write_data = ["Alpha","Beta","Gama"]
+
+
+# In[10]:
+
+
+with open ("Greekletters.txt","w") as g:
+    for value in write_data:
+        g.write(value)
+        g.write("\n")
+g.close()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
